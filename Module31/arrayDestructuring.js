4fc2a0eab0a335a7486c5c20a2246712574f0b71
BK -40 <bkhadiza277@gmail.com>
2021-09-01 18:56:23 +0600

    destructing

---
+++ Module31/arrayDestructuring.js
@@ -0,0 +1,9 @@
+// arrayDestructuring.js
+// declare variable based on the name of an Object porperty
+const myObject = {X:2, y:39, c:34, a:36};
+const {c,a} = myObject;
+console.log (b);
+
+// destructing array 
+const[p,q] = [45,37,91,8];
+console.log(q);
